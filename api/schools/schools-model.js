60590307590221d5eb078abf413911a9de5565d9
Imon Ovbude <imonovbude@gmail.com>
2019-03-18 14:43:18 -0700

    created get request and post request for schools

--- api/schools/schools-model.js
+++ api/schools/schools-model.js
@@ -1,9 +1,31 @@
 const db = require('../../data/dbConfig');
 
 module.exports = {
-    find
+    find,
+    findById,
+    insert
 }
 
-function find() {
-    return db('schools');
+function find(id) {
+    let query = db('schools');
+
+    if (id) {
+        return query
+            .where('id', id)
+            .first()
+    }
+
+    return query
+}
+
+function findById(id) {
+    return db('schools')
+        .where({ id })
+        .first()
+}
+
+async function insert(school) {
+    return db('schools')
+        .insert(school)
+        .then(([id]) => this.find(id))
 }
--- api/schools/schools-router.js
+++ api/schools/schools-router.js
@@ -2,14 +2,51 @@ const router = require('express').Router();
 
 const Schools = require('./schools-model');
 
+// GET REQUEST
+// GET ALL SCHOOLS
 router.get('/schools', (req, res) => {
     Schools.find()
         .then(schools => {
             res.status(200).json(schools)
         })
         .catch(error => {
-            res.status(500).json(error)
+            res.status(500).json({
+                error: 'There was a problem retrieving the schools.'
+            })
         })
 });
+
+// GET SCHOOL REQUEST
+router.get('/schools/:id', async (req, res) => {
+    try {
+        const school = await Schools.find(req.params.id)
+
+        if (school) {
+            res.status(200).json(school)
+        } else {
+            res.status(404).json({
+                message: 'The School was not found.'
+            })
+        }
+
+    } catch {
+        res.status(500).json({
+            error: 'There was a problem retrieving the school.'
+        })
+    }
+})
+
+// POST REQUEST
+// POST SCHOOL REQUEST
+router.post('/schools', async (req, res) => {
+    try {
+        const postSchool = await Schools.insert(req.body);
+        res.status(201).json(postSchool)
+    } catch {
+        res.status(500).json({
+            error: 'Create School failed'
+        })
+    }
+})
 
 module.exports = router;
