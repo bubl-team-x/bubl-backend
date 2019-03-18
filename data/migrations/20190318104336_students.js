73c7c783a4ae22d5f6484d222e3e907dba179879
Imon Ovbude <imonovbude@gmail.com>
2019-03-18 12:17:36 -0700

    added dynamic database environment, setup student table migration

---
+++ data/migrations/20190318104336_students.js
@@ -0,0 +1,19 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('students', function (students) {
+        students
+            .increments()
+
+        students
+            .string('student_name', 128)
+            .notNullable()
+
+        students
+            .string('school_name', 128)
+            .notNullable()
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('students');
+};
