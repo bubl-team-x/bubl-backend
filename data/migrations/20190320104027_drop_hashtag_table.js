0a096b7378d9c810d8cc44038f63d2dc443dc465
Imon Ovbude <imonovbude@gmail.com>
2019-03-20 10:44:52 -0700

    changed migration

--- data/migrations/20190320104027_drop_hashtag_table.js
+++ data/migrations/20190320104027_drop_hashtag_table.js
@@ -1,3 +1,14 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('hashtags', function (hashtags) {
+        hashtags
+            .increments()
+
+        hashtags
+            .string('description', 128)
+            .notNullable()
+            .unique()
+    })
+};
 
 exports.down = function (knex, Promise) {
     return knex.schema.dropTableIfExists('hashtags');
